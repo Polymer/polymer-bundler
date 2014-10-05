a314580c4aa94b1a3afd9c41df4c15746a1269a0
Pomax <pomax@nihongoresources.com>
2014-10-05 10:00:06 -0700

    PR fixes

--- test.js
+++ test.js
@@ -5,48 +5,50 @@
 
 var vulcan = require('./lib/vulcan.js');
 var fs = require('fs');
+var fail = function(err) {
+  console.error(err);
+  process.exit(1);
+};
+
 process.chdir('test');
 var data = fs.readFileSync('./import-test.html').toString();
 
+
 try { vulcan.process("lol"); process.exit(1); }
 catch (e) {
-  // this is supposed to throw an Error
+  // this is supposed to throw an Error, never
+  // reaching process.exit - continue the run.
 }
 
 vulcan.process(function(err,data) {
   // there should be an error here
   if (!err) {
-    console.error("Error: process was successfully called without any form of input. This should not happen.");
-    process.exit(1);
+    fail("Error: process was successfully called without any form of input. This should not happen.");
   }
 });
 
 vulcan.process("this", { input: "that.html" }, function(err,data) {
   // there should be an error here
   if (!err) {
-    console.error("Error: process was successfully called with both forms of input. This should not happen.");
-    process.exit(1);
+    fail("Error: process was successfully called with both forms of input. This should not happen.");
   }
 });
 
 vulcan.process(data, {input: "import-test.html", stdio: true}, function(err, data) {
   if(!err) {
-    console.error("Error: process was successfully called with both forms of input. This should not happen.");
-    process.exit(1);
+    fail("Error: process was successfully called with both forms of input. This should not happen.");
   }
 });
 
 vulcan.process(data, function(err, data) {
   if(err) {
-    console.error(err);
-    process.exit(1);
+    fail(err);
   }
 });
 
 vulcan.process({input: "import-test.html", stdio: true}, function(err, data) {
   if(err) {
-    console.error(err);
-    process.exit(1);
+    fail(err);
   }
 });
 
@@ -55,8 +57,7 @@ vulcan.process({input: "import-test.html"}, function(err, data) {
     fs.readFileSync("vulcanized.html");
     fs.unlinkSync("vulcanized.html");
   } catch (e) {
-    console.error("No vulcanized.html file was written despite options.stdio not being set.");
-    process.exit(1);
+    fail("No vulcanized.html file was written despite options.stdio not being set.");
   }
 });
 
