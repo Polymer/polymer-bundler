614eaeecb7ff3d8b9419c632796b0fb1c3a3225f
AJ Ortega <ajo@google.com>
2016-09-26 12:01:12 -0700

    update deep equality test

--- src/deps-index.ts
+++ src/deps-index.ts
@@ -32,58 +32,61 @@ type DependencyMapEntry = {
   lazy: Set<string>
 };
 
-async function _getDependencies(href: string, analyzer: Analyzer): Promise<DependencyMapEntry> {
-  const document = await analyzer.analyzeRoot(href);
-  const imports = document.getByKind('import');
-  const eagerImports = new Set<string>();
-  const lazyImports = new Set<string>();
-  for (let htmlImport of imports) {
-    if (!htmlImport.url) {
-      continue;
+async function _getDependencies(href: string, analyzer: Analyzer):
+    Promise<DependencyMapEntry> {
+      const document = await analyzer.analyzeRoot(href);
+      const imports = document.getByKind('import');
+      const eagerImports = new Set<string>();
+      const lazyImports = new Set<string>();
+      for (let htmlImport of imports) {
+        if (!htmlImport.url) {
+          continue;
+        }
+        switch (htmlImport.type) {
+          case 'html-import':
+            eagerImports.add(htmlImport.url);
+            break;
+          case 'lazy-html-import':
+            lazyImports.add(htmlImport.url);
+            break;
+        }
+      }
+      return {url: href, eager: eagerImports, lazy: lazyImports};
     }
-    switch (htmlImport.type) {
-      case 'html-import':
-        eagerImports.add(htmlImport.url);
-        break;
-      case 'lazy-html-import':
-        lazyImports.add(htmlImport.url);
-        break;
-    }
-  }
-  return {url: href, eager: eagerImports, lazy: lazyImports};
-}
 
-export async function buildDepsIndex(
-    entrypoints: string[], analyzer: Analyzer): Promise<DepsIndex> {
-  const entrypointToDependencies: Map<string, Set<string>> = new Map();
-  const dependenciesToEntrypoints: Map<string, Set<string>> = new Map();
-  let depsIndex = {};
-  const queue = entrypoints.slice();
-  const visitedEntrypoints = new Set<string>();
-  while (queue.length > 0) {
-    const entrypoint = queue.shift()!;
-    if (visitedEntrypoints.has(entrypoint)) {
-      continue;
-    }
-    const dependencyEntry = await _getDependencies(entrypoint, analyzer);
-    entrypointToDependencies.set(entrypoint, new Set(dependencyEntry.eager));
-    for (const lazyDependency of dependencyEntry.lazy.values()) {
-      if (!visitedEntrypoints.has(lazyDependency)) {
-        queue.push(lazyDependency);
+export async function buildDepsIndex(entrypoints: string[], analyzer: Analyzer):
+    Promise<DepsIndex> {
+      const entrypointToDependencies: Map<string, Set<string>> = new Map();
+      const dependenciesToEntrypoints: Map<string, Set<string>> = new Map();
+      let depsIndex = {};
+      const queue = entrypoints.slice();
+      const visitedEntrypoints = new Set<string>();
+      while (queue.length > 0) {
+        const entrypoint = queue.shift()!;
+        if (visitedEntrypoints.has(entrypoint)) {
+          continue;
+        }
+        const dependencyEntry = await _getDependencies(entrypoint, analyzer);
+        const dependencies = new Set(dependencyEntry.eager);
+        dependencies.add(entrypoint);
+        entrypointToDependencies.set(entrypoint, dependencies);
+        for (const lazyDependency of dependencyEntry.lazy.values()) {
+          if (!visitedEntrypoints.has(lazyDependency)) {
+            queue.push(lazyDependency);
+          }
+        }
       }
+      entrypointToDependencies.forEach((dependencies, entrypoint, map) => {
+        for (const dependency of dependencies) {
+          if (!dependenciesToEntrypoints.has(dependency)) {
+            dependenciesToEntrypoints.set(dependency, new Set());
+          }
+          const entrypointSet = dependenciesToEntrypoints.get(dependency)!;
+          entrypointSet.add(entrypoint);
+        }
+      });
+      return {
+        depsToFragments: dependenciesToEntrypoints,
+        fragmentToDeps: entrypointToDependencies
+      };
     }
-  }
-  entrypointToDependencies.forEach((dependencies, entrypoint, map) => {
-    for (const dependency of dependencies) {
-      if (!dependenciesToEntrypoints.has(dependency)) {
-        dependenciesToEntrypoints.set(dependency, new Set());
-      }
-      const entrypointSet = dependenciesToEntrypoints.get(dependency)!;
-      entrypointSet.add(entrypoint);
-    }
-  });
-  return {
-    depsToFragments: dependenciesToEntrypoints,
-    fragmentToDeps: entrypointToDependencies
-  };
-}
--- src/test/deps-index_test.ts
+++ src/test/deps-index_test.ts
@@ -47,7 +47,57 @@ suite('Bundler', () => {
   let analyzer: Analyzer;
   beforeEach(() => {
     analyzer = new Analyzer({urlLoader: new FSUrlLoader()});
-  })
+  });
+
+  const expectedDepsToFragments = new Map([
+    [common, new Set([common, endpoint1, endpoint2])],
+    [dep1, new Set([endpoint1, endpoint2])],
+    [dep2, new Set([endpoint2])],
+    [endpoint1, new Set([endpoint1, endpoint2])],
+    [endpoint2, new Set([endpoint2])],
+  ]);
+
+  const expectedFragmentToDeps = new Map([
+    [common, new Set([common])],
+    [endpoint1, new Set([common, dep1, endpoint1])],
+    [endpoint2, new Set([common, dep2, endpoint1, endpoint2, dep1])],
+  ]);
+
+  const deepMapSetEqual =
+      (actual: Map<string, Set<string>>,
+       expected: Map<string, Set<string>>) => {
+        // Check keys
+        const actualEntries = Array.from(actual.entries());
+        const expectedEntries = Array.from(expected.entries());
+        // Iterate and check values
+        const sortEntry = (a: any, b: any) => a[0].localeCompare(b[0]);
+        actualEntries.sort(sortEntry);
+        expectedEntries.sort(sortEntry);
+        console.log(actualEntries, expectedEntries);
+        if (actualEntries.length !== expectedEntries.length) {
+          throw new chai.AssertionError(
+              `Expected ${expectedEntries.length
+              } entries, got ${actualEntries.length} instead`);
+        }
+
+        for (let i = 0; i < actualEntries.length; i++) {
+          const actualEntry = actualEntries[i];
+          const expectedEntry = expectedEntries[i];
+          if (actualEntry[0] !== expectedEntry[0]) {
+            throw 'keys mismatched';
+          }
+          if (actualEntry[1].size !== expectedEntry[1].size) {
+            throw new chai.AssertionError(
+                `Wrong number of entries for key: ${actualEntry[0]}`);
+          }
+          for (const setEntry of actualEntry[1].values()) {
+            if (!expectedEntry[1].has(setEntry)) {
+              throw new chai.AssertionError(
+                  `Found unexpected key: ${setEntry}`);
+            }
+          }
+        }
+      }
 
   suite('Deps index tests', () => {
     test(
@@ -55,14 +105,8 @@ suite('Bundler', () => {
           return buildDepsIndex([common, endpoint1, endpoint2], analyzer)
               .then((index) => {
                 console.log(index);
-                assert.equal(index.depsToFragments.get(common)!.size, 2);
-                assert.equal(index.depsToFragments.get(dep1)!.size, 2);
-                assert.equal(index.depsToFragments.get(dep2)!.size, 1);
-                assert.equal(index.depsToFragments.get(endpoint1)!.size, 1);
-
-                assert.equal(index.fragmentToDeps.get(common)!.size, 0);
-                assert.equal(index.fragmentToDeps.get(endpoint1)!.size, 2);
-                assert.equal(index.fragmentToDeps.get(endpoint2)!.size, 4);
+                deepMapSetEqual(index.depsToFragments, expectedDepsToFragments);
+                deepMapSetEqual(index.fragmentToDeps, expectedFragmentToDeps);
               });
         });
   });
