2608f76962de39839bb8a17de70af97f61f026be
Alex Van Camp <vancamp.alex@gmail.com>
2017-05-24 12:35:30 -0500

    add test

---
+++ test/html/sourcemaps/external/babel-map.js
@@ -0,0 +1,2 @@
+console.log();
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImJhYmVsLW1hcC5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiQUFBQUEsUUFBUUMsR0FBUixFIiwiZmlsZSI6ImJhYmVsLW1hcC5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCk7XG4iXX0=
