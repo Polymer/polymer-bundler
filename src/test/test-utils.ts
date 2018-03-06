383d252948a730440803aea496b8ad4fb05f1926
Brendan Baldwin <brendan@usergenic.com>
2018-03-06 13:28:38 -0800

    Updated test-utils comments and added tests for the in memory analyzer and heredoc.

--- src/test/test-utils.ts
+++ src/test/test-utils.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+ * Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
  * This code may only be used under the BSD style license found at
  * http://polymer.github.io/LICENSE.txt
  * The complete set of authors may be found at
@@ -13,6 +13,20 @@
  */
 import {Analyzer, InMemoryOverlayUrlLoader, PackageRelativeUrl, PackageUrlResolver} from 'polymer-analyzer';
 
+/**
+ * Automatically left-justifies an indented multi-line template literal string
+ * so you can match your indent level with other surrounding source code.
+ *
+ * Example:
+ * ```typescript
+ *   const f = heredoc`
+ *     function f() {
+ *       something();
+ *     }
+ *   `;
+ *   assert(f === 'function f() {\n  something();\n}\n');
+ * ```
+ */
 export function heredoc(
     strings: TemplateStringsArray, ...values: any[]): string {
   let buildAString = '';
@@ -28,22 +42,22 @@ export function heredoc(
   return buildAString;
 }
 
-export function mindent(text: string): number {
-  const matches = text.match(/^ *(?=[^ \n])/mg) || [];
-  let mindent = null;
-  for (const match of matches) {
-    if (mindent === null || mindent > match.length) {
-      mindent = match.length;
-    }
-  }
-  return mindent === null ? 0 : mindent;
-}
-
-export function undent(text: string): string {
-  return text.replace(new RegExp(`^ {0,${mindent(text)}}`, 'mg'), '');
-}
-
-export function inMemoryAnalyzer(files: {[key: string]: string}) {
+/**
+ * Convenience function to build an Analyzer with a purely in-memory file map.
+ * Example:
+ * ```
+ * const analyzer = inMemoryAnalyzer({
+ *   'file1.html': `
+ *     <script src="./components/this-is-great/this-is-great.js"></script>
+ *     <this-is-great></this-is-great>
+ *   `,
+ *   `components/this-is-great/this-is-great.js': `
+ *     // something something custom elements
+ *   `,
+ * });
+ * ```
+ */
+export function inMemoryAnalyzer(files: {[key: string]: string}): Analyzer {
   const inMemoryLoader = new InMemoryOverlayUrlLoader();
   const urlResolver = new PackageUrlResolver({packageDir: '/memory/'});
   for (const packageUrl in files) {
@@ -56,4 +70,28 @@ export function inMemoryAnalyzer(files: {[key: string]: string}) {
   }
   const analyzer = new Analyzer({urlLoader: inMemoryLoader, urlResolver});
   return analyzer;
+}
+
+/**
+ * Returns the "minimum-indent level" which is the number of leading spaces on
+ * the non-empty line with the fewest leading spaces.  Used to know how many
+ * leading spaces to trim off of every line to left-justify multiline text.
+ */
+export function mindent(text: string): number {
+  const matches = text.match(/^ *(?=[^ \n])/mg) || [];
+  let mindent = null;
+  for (const match of matches) {
+    if (mindent === null || mindent > match.length) {
+      mindent = match.length;
+    }
+  }
+  return mindent === null ? 0 : mindent;
+}
+
+/**
+ * Left-justifies text in a multi-line indented string, but preserves relative
+ * indentation; `undent('  a\n    b')` returns `'a\n  b'`.
+ */
+export function undent(text: string): string {
+  return text.replace(new RegExp(`^ {0,${mindent(text)}}`, 'mg'), '');
 }
--- src/test/test-utils_test.ts
+++ src/test/test-utils_test.ts
@@ -11,13 +11,49 @@
  * subject to an additional IP rights grant found at
  * http://polymer.github.io/PATENTS.txt
  */
-/// <reference path="../../node_modules/@types/chai/index.d.ts" />
-/// <reference path="../../node_modules/@types/node/index.d.ts" />
-/// <reference path="../../node_modules/@types/mocha/index.d.ts" />
 import {assert} from 'chai';
-import {mindent, undent} from './test-utils';
+
+import {heredoc, inMemoryAnalyzer, mindent, undent} from './test-utils';
 
 suite('test-utils', () => {
+
+  suite('inMemoryAnalyzer', () => {
+
+    test('can load the provided string literals as files', async () => {
+      const analyzer = inMemoryAnalyzer({
+        'index.html': `
+        <link rel="import" href="components/cool-element/cool-element.html">
+        <cool-element></cool-element>
+      `,
+        'components/cool-element/cool-element.html': `
+        <!-- something something custom elements -->
+      `,
+      });
+      const indexUrl = analyzer.resolveUrl('index.html')!;
+      const elementUrl =
+          analyzer.resolveUrl('components/cool-element/cool-element.html')!;
+      const analysis = await analyzer.analyze([indexUrl, elementUrl]);
+      const indexResult = analysis.getDocument(indexUrl);
+      assert.equal(indexResult.successful, true);
+      const indexDocument = indexResult.successful && indexResult.value;
+      assert.deepEqual(
+          indexDocument && indexDocument.parsedDocument.contents,
+          '<link rel="import" href="components/cool-element/cool-element.html">\n' +
+              '<cool-element></cool-element>\n');
+    });
+  });
+
+  suite('heredoc', () => {
+
+    test('fixes indent level', () => {
+      assert.deepEqual(heredoc`
+          check
+
+        this
+            out
+      `, '  check\n\nthis\n    out\n');
+    });
+  });
 
   suite('mindent', () => {
 
