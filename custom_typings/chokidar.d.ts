f6927fec523867330ee8c427ec386b3654174d29
Brendan Baldwin <brendan@usergenic.com>
2018-03-06 10:51:52 -0800

    Added @types/chokidar after previously thinking that didnt exist.

--- custom_typings/chokidar.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This file exists to satisfy a TypeScript check that otherwise reports this
- * error, even though we don't actually use the watch option for rollup.
- */
-declare module 'chokidar' {
-  interface WatchOptions {}
-  export {WatchOptions};
-}
